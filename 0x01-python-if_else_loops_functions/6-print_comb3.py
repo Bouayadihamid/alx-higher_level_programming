0082c127427cd9b7df38da6e789dd1354bc99b4c
Hamid <Bouayadihamid@gmail.com>
2023-11-28 15:22:53 -0800

    Combinitons of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for num in range (10):
+    for i in range(num, 10):
+        if num < i:
+            print("{:d}{:d}".format(num, i), end="\n" if num == 8 and i == 9 else ", ")
