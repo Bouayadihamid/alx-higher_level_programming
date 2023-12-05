c48a5f3517bb0c712f446dc9149022546ebda355
Hamid <Bouayadihamid@gmail.com>
2023-12-04 17:26:54 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if not sentence:
         return None
     else:
-        return (len(sentence), sentence[0])
+        if len(sentence) > 0:
+            return (len(sentence), sentence[0])
