134fab42214de88b1a88c89ce3e9f44f2f239b7f
Hamid <Bouayadihamid@gmail.com>
2023-12-04 17:54:51 -0800

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    my_result= []
+    for i in my_list:
+        if i % 2 == 0:
+            my_result.append(True)
+        else:
+            my_result.append(False)
+    return my_result
