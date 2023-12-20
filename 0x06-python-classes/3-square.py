b7daedf1c2bc231b9cf095eb9bca4bb2e4b57ae1
Hamid <Bouayadihamid@gmail.com>
2023-12-19 17:22:53 -0800

    Square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    """A square."""
+
+    def __init__(self, size=0):
+        """Hamid.
+
+        Args:
+            size: The length of a side of the square.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+        """Square Area.
+
+        Returns:
+            The square size.
+        """
+        return self.__size ** 2
