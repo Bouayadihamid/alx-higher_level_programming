769e74ee079c1ab8c319adda52286615dd50271d
Hamid <Bouayadihamid@gmail.com>
2023-12-20 17:24:17 -0800

    Safe print integer

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     NewList = []
+
     for i in range(list_length):
         try:
             NewList.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             NewList.append(0)
             print("division by 0")
+            continue
+        except IndexError:
+            NewList.append(0)
+            print("out of range")
             continue
         except TypeError:
             NewList.append(0)
