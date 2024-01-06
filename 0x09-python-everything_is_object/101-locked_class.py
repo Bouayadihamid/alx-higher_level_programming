7efdb302e6287d4e61b2b093ab57a235efda71bc
Hamid <Bouayadihamid@gmail.com>
2024-01-06 14:51:35 -0800

    Magic string

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,9 +4,8 @@
 
 class LockedClass:
     """
-    prevents the user from dynamically creating new instance 
-    attributes, except if the new instance attribute is 
+    prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is
     called first_name.
     """
-
     __slots__ = ["first_name"]
