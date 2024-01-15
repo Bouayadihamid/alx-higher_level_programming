48f98c9346ad6a2ea9e8eafcb83946ade4e98552
Hamid <Bouayadihamid@gmail.com>
2024-01-15 15:32:08 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module for the base class.'''
+
+
+class Base:
+    '''The Base representation'''
+
+    __nb_objects = 0
+
+    def def __init__(self, id=None):
+        '''Constructor'''
+        if id is not None:
+            self.id = is
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
