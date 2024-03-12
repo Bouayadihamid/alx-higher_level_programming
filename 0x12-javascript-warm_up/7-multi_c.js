ecd18eaa7cd796f77b280243bfd4ebb516b37391
Hamid <Bouayadihamid@gmail.com>
2024-03-12 13:14:33 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (!args) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = parseInt(args);
+  if (isNaN(x)) {
+	console.log('Missing number of occurrences');
+} else {
+	for (let i = 0; i < x; i++) {
+		console.log('C is fun');
+	}
+}
+}
