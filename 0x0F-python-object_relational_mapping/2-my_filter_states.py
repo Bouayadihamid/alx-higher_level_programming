b149f10afef756cde99aa626f4a71441c4d42ad6
Hamid <Bouayadihamid@gmail.com>
2024-03-16 19:29:24 -0700

    correct code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument"""
+
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-    #state_name = sys.argv[4]
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC;".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
