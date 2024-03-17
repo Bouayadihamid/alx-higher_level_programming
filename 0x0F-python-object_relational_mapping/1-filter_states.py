8256d6fdd1ac3c090369f7d81d57b7d50f18094c
Hamid <Bouayadihamid@gmail.com>
2024-03-16 19:10:40 -0700

    fixed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that lists all states with a name starting with N (upper N}
+"""a script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa:"""
 
 import sys
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
 
     states = cur.fetchall()
     for state in states:
