f8599736f865eff7b492842d6f497900a1d9d7b4
Hamid <Bouayadihamid@gmail.com>
2024-05-02 18:55:55 -0700

    Changes

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,13 +1,7 @@
-#!/usr/bin/nod
-/* Fetches and prints how to say “Hello” depending
-on the language */
+#!/usr/bin/node
+/* Changes the color of an element by id. */
 $(document).ready(function () {
-  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
-  $('#btn_translate').click(function () {
-    $('#hello').empty();
-    const lang = $('#language_code').val();
-    $.get(url + lang, function (data) {
-      $('#hello').append(data.hello);
-    });
+  $('#red_header').click(function () {
+    $("header").css('color', '#FF0000');
   });
 });
--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 /* Toggles the class of the <header> element when
 the user clicks on the tag DIV#toggle_header. */
-$(document).ready(function () {
-  $('#toggle_header').click(function () {
-    if ($('header').hasClass('red')) {
-      $('header').removeClass('red').addClass('green');
-    } else {
-      $('header').removeClass('green').addClass('red');
-    }
-  });
+$(document).ready(function(){
+        $("#toggle_header").click(function(){
+            $("header").toggleClass("green red");
+        });
 });
